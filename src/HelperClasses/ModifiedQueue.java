b157fa0d9d2c33bdf8cdd490ec317f907b8c1c49
Yonatan Dotan <yonadot11@gmail.com>
2023-06-20 02:06:11 +0300

    Set up the ModifiedQueue class for question four

--- src/HelperClasses/ModifiedQueue.java
+++ src/HelperClasses/ModifiedQueue.java
@@ -1,2 +1,46 @@
-package HelperClasses;public class ModifiedQueue {
+package HelperClasses;
+
+import java.util.AbstractQueue;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class ModifiedQueue<T> extends AbstractQueue<T> {
+    private LinkedList<T> elements;
+
+    public ModifiedQueue() {
+        this.elements = new LinkedList<T>();
+    }
+    @Override
+    public Iterator<T> iterator() {
+        return elements.iterator();
+    }
+
+    @Override
+    public int size() {
+        return elements.size();
+    }
+
+    @Override
+    public boolean offer(T t) {
+        if(t == null) return false;
+        elements.add(t);
+        return true;
+    }
+
+    @Override
+    public T poll() {
+        Iterator<T> iter = elements.iterator();
+        T t = iter.next();
+        if(t != null){
+            iter.remove();
+            return t;
+        }
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return elements.getFirst();
+    }
+
 }
